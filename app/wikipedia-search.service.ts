bf1bdd2fb3b8199393c611e3c41357c98e694d2b
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-09-21 07:47:40 +0100

    Hide rxjs implementation in service

--- app/wikipedia-search.service.ts
+++ app/wikipedia-search.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { URLSearchParams, Jsonp } from '@angular/http';
 import 'rxjs/add/operator/delay';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class WikipediaSearchService {
     constructor(private jsonp: Jsonp) {}
 
-    search(term: string) {
+    search(terms: Observable<string>, debounceMs=400) {
+        return terms.debounceTime(400)
+                    .distinctUntilChanged()
+                    .switchMap(term => this.rawsearch(term)) //flattens observable of observable
+    }
+
+    rawsearch(term: string) {
         let search = new URLSearchParams();
         search.set('action', 'opensearch');
         search.set('search', term);
