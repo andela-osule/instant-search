5003d2ca6c85daebf714c92fcf8b3a5710757fa5
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-09-21 07:23:58 +0100

    Debounce time for 400 ms

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { WikipediaSearchService } from './wikipedia-search.service';
 import { Subject } from 'rxjs/subject';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
 
 
 @Component({
@@ -15,7 +16,8 @@ export class AppComponent {
     items: Array<string>;
     term$ = new Subject<string>();
     constructor(private service: WikipediaSearchService) {
-        this.term$.subscribe(term => this.search(term));
+        this.term$.
+        debounceTime(400).subscribe(term => this.search(term));
     }
 
     search(term: string) {
